c3f1beb0e007a5b3b690233470662cf2cc6fa6e3
MinJi Choi <sistar96@gmail.com>
2023-10-18 11:25:37 +0900

    Fileoutput

--- Basic/src/main/java/basic05/MessageBeanImpl.java
+++ Basic/src/main/java/basic05/MessageBeanImpl.java
@@ -1,9 +1,15 @@
 package basic05;
 
+import java.io.IOException;
+
 public class MessageBeanImpl implements MessageBean{
 	private String name;
 	private int age;
 	private String greeting;
+	
+	/* 설정파일에 만들어 놓은 것을 자동적으로 주입 받을 수 있게 변수를 추가 */
+	private Outputter outputter;
+	
 	
 	public MessageBeanImpl() {}
 	public MessageBeanImpl(String name, int age, String greeting) {
@@ -23,10 +29,21 @@ public class MessageBeanImpl implements MessageBean{
 	public void setGreeting(String greeting) {
 		this.greeting = greeting;
 	}
-
+	
+	public void setOutputter(Outputter outputter) {
+		this.outputter = outputter;
+	}
+	
 	@Override
 	public void sayHello() {
 		String msg = greeting + "!~~" + name + "님~~ 이제 당신은 " + age +  "살 입니다.";
 		System.out.println(msg);
+		
+		// 파일로 출력이 되게끔
+		try {
+			outputter.output(msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
