f6500b058c3fd4b86ff7b6be76cac054d96da09d
MinJi Choi <sistar96@gmail.com>
2023-10-18 10:51:12 +0900

    생성자와 setter based injection

---
+++ Basic/src/main/java/basic05/MessageBean.java
@@ -0,0 +1,6 @@
+package basic05;
+
+interface MessageBean {
+	// 메세지를 출력하기 위한
+	public void sayHello();
+}
---
+++ Basic/src/main/java/basic05/Outputter.java
@@ -0,0 +1,8 @@
+package basic05;
+
+import java.io.IOException;
+
+// 어떤 장치로 출력한 것인가를 상속받아서 쓸 수 있는 인터페이스
+public interface Outputter {
+	void output(String msg) throws IOException;
+}
