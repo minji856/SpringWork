420cbb0fe25c98d17a4951dd589914ce4a8ed7f2
MinJi Choi <sistar96@gmail.com>
2023-10-27 17:21:12 +0900

    sql분

--- finalproject/src/main/java/com/exam/domain/query.sql
+++ finalproject/src/main/java/com/exam/domain/query.sql
@@ -1,11 +1,27 @@
 CREATE TABLE simpleBoard(
-	bNo		number not null,
-	bTitle 	varchar2(200) not null,
-	bContent varchar2(4000)  null,
-	bWriter varchar2(50) not null,
-	bRegdate timestamp default sysdate not null,
+	bNo			number not null,
+	bTitle 		varchar2(200) not null,
+	bContent 	varchar2(4000)  null,
+	bWriter		varchar2(50) not null,
+	bRegdate 	timestamp default sysdate not null,
 	bViewcnt	number default 0,
 	CONSTRAINT pk_id primary key(bNO)
 );
 
-drop table simpleBoard;
+--drop table simpleBoard;
+
+CREATE TABLE simpleMember(
+	id			number	not null,
+	email		varchar2(50),
+	password	varchar2(50),
+	name		varchar2(50),
+	regdate		date,
+	CONSTRAINT pk_email PRIMARY KEY(email)
+);
+
+CREATE SEQUENCE seq_bno;
+CREATE SEQUENCE seq_id;
+
+--DROP sequence seq_id;
+
+SELECT * FROM simpleBoard;
