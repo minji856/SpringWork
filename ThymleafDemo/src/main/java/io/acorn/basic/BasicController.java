6131f00b097b32ad08fcaa2c74e44e52ef3db986
MinJi Choi <sistar96@gmail.com>
2023-11-02 17:35:47 +0900

    타임리프 문법들

--- ThymleafDemo/src/main/java/io/acorn/basic/BasicController.java
+++ ThymleafDemo/src/main/java/io/acorn/basic/BasicController.java
@@ -1,16 +1,20 @@
 package io.acorn.basic;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.Getter;
 
 @Controller
@@ -54,5 +58,83 @@ public class BasicController {
 			this.userName = userName;
 			this.age = age;
 		}
+	}
+//	기본 객체(Request)에 담아서 전달
+	@GetMapping("/basic-objects")
+	public String basicObject(HttpSession session,
+							HttpServletRequest req) {
+		session.setAttribute("sessionData", "Hello Session~");
+		req.setAttribute("userName", "홍길동");
+		
+		return "basic/basic-objects";
+	}
+	
+	@Component("helloBean")
+	static class HelloBean{
+		public String hello(String data) {
+			return "Hello!!! " + data; 
+		}
+	}
+	
+	@GetMapping("/date")
+	public String date(Model model) {
+		model.addAttribute("localDateTime", LocalDateTime.now());
+		return "basic/date"; 
+	}
+	
+	@GetMapping("/link")
+	public String link(Model model) {
+		model.addAttribute("param1", "data1");
+		model.addAttribute("param2", "data2");
+		return "basic/link"; 
+	}
+	
+	@GetMapping("/literal")
+	public String literal(Model model) {
+		model.addAttribute("data", "Spring~~");
+		return "basic/literal";
+	}
+	
+	@GetMapping("/operation")
+	public String operation(Model model) {
+		model.addAttribute("nullData", null);
+		model.addAttribute("data", "Spring~~");
+		return "basic/operation";
+	}
+	
+	@GetMapping("/attribute")
+	public String attribute() {
+		return "basic/attribute";
+	}
+	
+	@GetMapping("/each")
+	public String each(Model model) {
+		addUsers(model);
+		return "basic/each";
+	}
+	
+	@GetMapping("/condition")
+	public String condition(Model model) {
+		addUsers(model);
+		return "basic/condition";
+	}
+
+	private void addUsers(Model model) {
+		List<User> users = Arrays.asList(new User("userA", 10),
+									new User("userB", 20),
+									new User("userC", 30));
+		model.addAttribute("users" , users);
+	}
+	
+	@GetMapping("/comments")
+	public String comments(Model model) {
+		model.addAttribute("data", "Spring!~~");
+		return "basic/comments";
+	}
+	
+	@GetMapping("/block")
+	public String block(Model model) {
+		addUsers(model);
+		return "basic/block";
 	}
 }
