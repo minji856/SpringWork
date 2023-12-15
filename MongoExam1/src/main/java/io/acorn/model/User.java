3270f495021d2d92d17317bc34fe1fea0d484de9
MinJi Choi <sistar96@gmail.com>
2023-12-15 13:15:18 +0900

    Mongodb 서버 접속

---
+++ MongoExam1/src/main/java/io/acorn/model/User.java
@@ -0,0 +1,15 @@
+package io.acorn.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection="users")
+public class User {
+	@Id
+	private String id;
+	private String name;
+	private int age;
+}
