4aee3fae42035b00d962bbe3e81c2141bc704da5
MinJi Choi <sistar96@gmail.com>
2023-12-20 12:11:27 +0900

    이름과 날짜로 조회& Answer Question Entity

--- JpaExam2/src/main/java/io/acorn/UsersService.java
+++ JpaExam2/src/main/java/io/acorn/UsersService.java
@@ -6,28 +6,34 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import io.acorn.model.Users;
+import io.acorn.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Service는 이름 내맘대로 만들어도됨
  */
 @Service
+@RequiredArgsConstructor
 public class UsersService {
-	// 성과 이름으로 조회를 한다. 단 userId로 내림차순 정렬하고 결과는 5개만 가져온다.
-	List<Users> getByName(String last, String frist){
-		return null;
+	private final UsersRepository usersRepository;
+	
+	// 규칙에 의해만든 쿼리메서드 호출
+	public List<Users> getByName(String last, String frist){
+		return usersRepository.findFirst5ByLastnameAndFirstnameOrderByUserIdDesc(last, frist);
 	}
 	
 	// 대소문자 무시하고 Like쿼리를 이용하여 해당하는 데이터 갯수를 가져온다.
-	int getByNameLike(){
-		return 0;
+	public int getByNameLike(String frist){
+		return usersRepository.countByFirstnameIgnoreCaseLike(frist);
 	}
 	
 	// 입력받은 날짜보다 startDate가 적거나 같은 데이터가 있다면 true를, 없다면 false를 리턴
-	boolean getDate(LocalDateTime date) {
-		return true;
+	public boolean getDate(LocalDateTime date) {
+		return usersRepository.existsByStartDateLessThanEqual(date);
 	}
 	
-	public void createUsers(Users user) {
-		
+	public String createUsers(Users user) {
+		usersRepository.save(user);
+		return "등록 완료";
 	}
 }
