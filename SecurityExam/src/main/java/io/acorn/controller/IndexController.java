7451223baaa45b27c0c0c3d47d444d86cca58a30
MinJi Choi <sistar96@gmail.com>
2023-12-21 14:30:29 +0900

    로그인권한Security

--- SecurityExam/src/main/java/io/acorn/controller/IndexController.java
+++ SecurityExam/src/main/java/io/acorn/controller/IndexController.java
@@ -1,12 +1,79 @@
 package io.acorn.controller;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.acorn.model.Users;
+import io.acorn.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
-	@GetMapping
+	private final UsersRepository usersRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder; // 관례상 소문자로 변수이름 지어줌
+	
+	@GetMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	
+	@GetMapping("/user")
+	@ResponseBody
+	public String user() {
+		return "user";
+	}
+
+	@GetMapping("/admin")
+	@ResponseBody
+	public String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	@ResponseBody
+	public String manager() {
+		return "manager";
+	}
+	
+	@PostMapping("/join")
+	public String join(Users user) {
+		System.out.println("user : " + user);
+		user.setRole("ROLE_USER");
+		
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		// 변수에 저장한것을 다시 객체에 넣어준다
+		user.setPassword(encPassword);
+		
+		usersRepository.save(user);
+		
+		// 자동으로 로그인 페이지로 이동
+		return "redirect:/loginForm";
+	}
+	
+	@GetMapping("/joinProc")
+	@ResponseBody
+	public String joinProc() {
+		return "회원가입 완료됨";
+	}
+	
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
+	}
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
+	}
+	
+	@PostMapping("/login")
+	public String login(){
+		return "redirect:/";
 	}
 }
